b1884e43b11301fc8e7c2c609c296ff1609b5fae
Sven Herzberg <herzberg.sven@googlemail.com>
2012-06-25 10:42:19 +0200

    implement a wrapper around link()

--- hlink.c
+++ hlink.c
@@ -1,11 +1,30 @@
 /* FIXME: add MPL2 header */
 
-#include <stdlib.h>
+#include <stdlib.h> /* EXIT_SUCCESS */
+#include <stdio.h>  /* fprintf() */
+#include <unistd.h> /* link */
 
 int
-main (int   argc __attribute__((__unused__)),
-      char**argv __attribute__((__unused__)))
+main (int   argc,
+      char**argv)
 {
+    int status;
+
+    if (argc != 3) {
+        fprintf(stderr,
+                "%s:%d:%s(): implement help screen\n",
+                __FILE__,
+                __LINE__,
+                __FUNCTION__);
+        return EXIT_FAILURE;
+    }
+
+    status = link(argv[1], argv[2]);
+    if (status < 0) {
+        perror("cannot create the link");
+        return EXIT_SUCCESS;
+    }
+
     return EXIT_SUCCESS;
 }
 
